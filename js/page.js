633a78a9814efecb0c5172d748dbfe88ec91bca0
Rita <sherbar@clarkson.edu>
2019-11-05 10:45:05 -0500

    Initial commit

---
+++ js/page.js
@@ -0,0 +1,10 @@
+//* Page Scripts
+// Mobile Navigaton Toggle
+function navToggle() { // creates a new function named navToggle
+  var x = document.getElementById("main-nav"); // creates a variable set to #main-nav
+    if (x.className === "topnav") { // if #main-nav has a class of .topnav,
+    x.className += " responsive";   // then add a new class
+  } else {                          // if it doesn't,
+    x.className = "topnav";         // set class to .topnav
+  } //closes if-else
+} //closes function
